1a391cd8cb6f1caab96f06097a36ca34930c4751
OttoDan <33697887+OttoDan@users.noreply.github.com>
2018-01-26 22:52:07 +0100

    added buggy jump

--- GGJ_Prototype/Assets/Scripts/CharacterMovement.cs
+++ GGJ_Prototype/Assets/Scripts/CharacterMovement.cs
@@ -3,6 +3,8 @@
 public class CharacterMovement : MonoBehaviour {
     private Rigidbody rb;
     public float movementSpeed = 0.1f;
+    public float jumpForce = 8f;
+    public bool isGrounded = false;
     // Use this for initialization
     void Start () {
         rb = GetComponent<Rigidbody>();
@@ -11,6 +13,29 @@ public class CharacterMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        rb.velocity += Input.GetAxis("Horizontal") * Vector3.right * movementSpeed;
+        if (rb.velocity.y < 0.5f && rb.velocity.y > -0.5f) { 
+            rb.velocity += Input.GetAxisRaw("Horizontal") * Vector3.right * movementSpeed;
+        
+            if (Input.GetButtonDown("Jump"))
+            {
+                rb.velocity += Vector3.up * jumpForce;
+            }
+        }
 	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            isGrounded = false;
+        }
+    }
 }
