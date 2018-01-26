2dc56b1d4e1acff8e1021724ac3c9fb8663b47ea
OttoDan <33697887+OttoDan@users.noreply.github.com>
2018-01-26 21:56:36 +0100

    Added MovementBaiscScript

---
+++ GGJ_Prototype/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour {
+    private Rigidbody rb;
+    public float movementSpeed = 0.1f;
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb.velocity += Input.GetAxis("Horizontal") * Vector3.right * movementSpeed;
+	}
+}
