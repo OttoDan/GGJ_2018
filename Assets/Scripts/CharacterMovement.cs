d7e53a593865a3d221a81b35a1db87ecbd8789d9
HerrBrunke <34299221+HerrBrunke@users.noreply.github.com>
2018-01-27 06:32:37 +0100

    TestSceneFalle_O1

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -1,41 +1,8 @@
 ﻿using UnityEngine;
 
-public class CharacterMovement : MonoBehaviour {
-    private Rigidbody rb;
-    public float movementSpeed = 0.1f;
-    public float jumpForce = 8f;
-    public bool isGrounded = false;
-    // Use this for initialization
-    void Start () {
-        rb = GetComponent<Rigidbody>();
+public class CharacterMovement : MonoBehaviour
+{
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (rb.velocity.y < 0.5f && rb.velocity.y > -0.5f) { 
-            rb.velocity += Input.GetAxisRaw("Horizontal") * Vector3.right * movementSpeed;
-        
-            if (Input.GetButtonDown("Jump"))
-            {
-                rb.velocity += Vector3.up * jumpForce;
-            }
-        }
-	}
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if(collision.gameObject.tag == "Ground")
-        {
-            isGrounded = true;
-        }
-    }
 
-    private void OnCollisionExit(Collision collision)
-    {
-        if(collision.gameObject.tag == "Ground")
-        {
-            isGrounded = false;
-        }
-    }
 }
