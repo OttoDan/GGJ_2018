1d2f31c6eb05cf063f6bbb4569c1193199fd0f37
OttoDan <33697887+OttoDan@users.noreply.github.com>
2018-01-27 08:55:16 +0100

    Added Death Physic pseudo Ragdoll

---
+++ Assets/Scripts/CharacterHealth.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterHealth : MonoBehaviour {
+
+    public void Kill()
+    {
+        GameObject parent = gameObject;
+
+        Transform[] allChildren = GetComponentsInChildren<Transform>();
+        foreach (Transform child in allChildren)
+        {
+            if(child.gameObject.GetComponent<SpriteRenderer>() != null)
+            {
+                child.gameObject.AddComponent<BoxCollider2D>();
+                child.gameObject.AddComponent<Rigidbody2D>();
+                child.gameObject.GetComponent<Rigidbody2D>().velocity += Vector2.up *  Random.Range(2f, 12f);
+                child.gameObject.GetComponent<Rigidbody2D>().velocity += Vector2.right * Random.Range(-12f,12f);
+                child.parent = null;
+
+            }
+            
+        }
+
+        //for(int i=0; i< transform.childCount; i++)
+        //{
+        //    transform.GetChild(i).gameObject.AddComponent<BoxCollider2D>();
+        //    transform.GetChild(i).gameObject.AddComponent<Rigidbody2D>();
+        //}
+        //Transform[] allChildren = GetComponentsInChildren<Transform>();
+        //foreach (Transform child in allChildren)
+        //{
+        //    child.gameObject.AddComponent<BoxCollider2D>();
+        //    child.gameObject.AddComponent<Rigidbody2D>();
+        //}
+        //foreach (Transform child in transform)
+        //{
+        //    child.gameObject.AddComponent<BoxCollider2D>();
+        //    child.gameObject.AddComponent<Rigidbody2D>();
+        //}
+        //Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/TrapTestScript2.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapTestScript2 : MonoBehaviour {
+    public Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+        rb.simulated = false;
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        rb.simulated = true;
+    }
+}
