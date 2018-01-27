6fbeeb33af7b3f15d0db81e257afddde5578245d
OttoDan <33697887+OttoDan@users.noreply.github.com>
2018-01-27 23:35:45 +0100

    tund

--- Assets/Scripts/CharacterHealth.cs
+++ Assets/Scripts/CharacterHealth.cs
@@ -7,15 +7,17 @@ public class CharacterHealth : MonoBehaviour
     public bool dead = false;
     public GameObject characterPrefab;
     public Transform safeSpot;
+    Transform[] allChildren;
     private void Update()
     {
         if (dead)
         {
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (Input.GetKeyUp(KeyCode.Space))
             {
+                
                 if (safeSpot != null)
                 {
-                    Instantiate(characterPrefab, safeSpot.position, Quaternion.identity);
+                    GameObject.Instantiate(characterPrefab, safeSpot.position,Quaternion.identity);//, Quaternion.identity);
                     Destroy(gameObject);
                 }
             }
@@ -23,19 +25,25 @@ public class CharacterHealth : MonoBehaviour
     }
     public void Kill()
     {
-        GameObject parent = gameObject;
-
-        Transform[] allChildren = GetComponentsInChildren<Transform>();
+        //GameObject parent = gameObject;
+        allChildren = GetComponentsInChildren<Transform>();
+        Destroy(GetComponent<playerMoveFinish>());
         foreach (Transform child in allChildren)
         {
-            if(child.gameObject.GetComponent<SpriteRenderer>() != null)
+            
+            if (child.gameObject.GetComponent<SpriteRenderer>() != null)
             {
+                
                 child.gameObject.AddComponent<BoxCollider2D>();
                 child.gameObject.AddComponent<Rigidbody2D>();
                 child.gameObject.GetComponent<Rigidbody2D>().velocity += Vector2.up *  Random.Range(2f, 12f);
                 child.gameObject.GetComponent<Rigidbody2D>().velocity += Vector2.right * Random.Range(-12f,12f);
-                Destroy(GetComponent<CharacterMovementAnimation>());
+
                 child.parent = null;
+            }
+            else
+            {
+                child.gameObject.AddComponent<KillLeftovers>();
             }
             
         }
--- Assets/Scripts/ClimbTrigger.cs
+++ Assets/Scripts/ClimbTrigger.cs
@@ -4,16 +4,32 @@ using UnityEngine;
 
 public class ClimbTrigger : MonoBehaviour {
     public Transform destination;
+    public bool climbing = false;
     
     private void OnTriggerStay2D(Collider2D collision)
     {
         if(collision.tag == "Player")
         {
-            if (Input.GetKey(KeyCode.E))
+            if (climbing)
             {
-                //collision.transform.position = destination.position;
-                collision.GetComponent<Animator>().SetFloat("climb", 10f);
+                Debug.Log(collision.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length);
+                collision.GetComponent<Animator>().SetBool("climb", false);
+                
+                if (collision.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length > 1.1f
+                    )
+                {
+                    // collision.transform.position = destination.position;
+                    climbing = false;
+                }
             }
+            else if (Input.GetKeyDown(KeyCode.E))
+            {
+                collision.GetComponent<Rigidbody2D>().AddForce(Vector2.up*12f);
+                collision.GetComponent<Animator>().SetBool("climb",true);
+                //collision.GetComponent<Animator>().Play("climb", -1, 0f);
+                climbing = true;
+            }
+
         }
     }
 }
