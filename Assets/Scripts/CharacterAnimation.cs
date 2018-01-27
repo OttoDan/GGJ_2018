a91682ef93daed1d15f5358f3f28992ebea2f6dc
OttoDan <33697887+OttoDan@users.noreply.github.com>
2018-01-27 01:35:18 +0100

    added simple animation script

---
+++ Assets/Scripts/CharacterAnimation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimation : MonoBehaviour {
+    Animator anim;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float move = Input.GetAxis("Horizontal");
+        anim.SetFloat("speed", move*1.0f);
+	}
+}
