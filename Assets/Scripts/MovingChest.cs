f0da7f52182a17e808755b3908373957a0a423d4
JannikFunke <jannik.f@online.de>
2018-01-28 06:07:14 +0100

    we3we

--- Assets/Scripts/MovingChest.cs
+++ Assets/Scripts/MovingChest.cs
@@ -5,23 +5,33 @@ using UnityEngine;
 public class MovingChest : MonoBehaviour {
     public Vector3 scale;
     public Vector3 direction;
+    public Collider2D wer;
     private void Start()
     {
         scale = transform.localScale;
+        wer = GetComponent<Collider2D>();
+
+
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
         if(collision.tag == "Player")
         {
+           
             direction = transform.position - collision.transform.position;
             direction.Normalize();
             if (Input.GetKey(KeyCode.E))
             {
-                transform.position = collision.transform.position+ direction * 2f;
+                wer.enabled = false;
+                transform.position = collision.transform.position+ direction * 1f;
+                
+               // gameObject.layer = 11;
             }
             else
             {
                 transform.parent = null;
+                wer.enabled = true;
+              //  gameObject.layer = 9;
             }
         }
     }
